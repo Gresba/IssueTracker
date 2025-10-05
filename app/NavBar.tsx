0c712d640e7ff249a645a11eadad8fafa933c4ab
Paul Kim <kimpaul0709@gmail.com>
2025-10-04 21:13:48 -0400

    Created issue Model

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,19 @@
-import Link from 'next/link'
-import React from 'react'
+'use client'
 
-import { FaBug } from "react-icons/fa";
+import { usePathname }  from 'next/navigation';
+
+import { FaBug }        from "react-icons/fa";
+
+import classNames       from 'classnames';
+import Link             from 'next/link'
+import React            from 'react'
 
 const NavBar = () => {
+    const currentPath = usePathname();
+
     const links = [
         { label: 'Dashboard', href: '/'},
-        { label: 'Issues', href: '/'}
+        { label: 'Issues', href: '/issues'}
     ]
 
     return (
@@ -16,7 +23,14 @@ const NavBar = () => {
                 {links.map(link => 
                     <Link
                         key={link.href}
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                        className={
+                            classNames(
+                                {
+                                    'text-zinc-900': link.href === currentPath,
+                                    'text-zinc-500': link.href !== currentPath,
+                                    'hover:text-zinc-800 transition-colors': true
+                                }
+                            )} 
                         href={link.href}>{link.label}</Link>
                 )}
             </ul>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import NavBar from "./NavBar";
 
 const geistSans = Geist({
