75e33007edfcaadfc718905ab370058eca3301b1
Paul Kim <kimpaul0709@gmail.com>
2025-10-05 14:31:00 -0400

    Handle Form Submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,39 @@
 'use client'
 
-import { Button, TextField } from '@radix-ui/themes'
-import SimpleMDE from 'react-simplemde-editor'
-import { useState } from 'react'
+import { Button, TextField }    from '@radix-ui/themes'
+import { useForm, Controller }  from 'react-hook-form';
+import { useRouter }            from 'next/navigation';
+import SimpleMDE                from 'react-simplemde-editor'
+import axios                    from 'axios'
 import "easymde/dist/easymde.min.css";
 
+interface IssueForm {
+    title: string;
+    description: string;
+}
+
 const NewIssuesPage = () => {
-    const [value, setValue] = useState('')
+    const router = useRouter();
+    const { register, control, handleSubmit } = useForm<IssueForm>();
 
     return (
-        <div className='max-w-xl space-y-3'>
-            <TextField.Root placeholder='Title'></TextField.Root>
-            <SimpleMDE 
-                value={value} 
-                onChange={setValue}
-                />
+        <form 
+            className='max-w-xl space-y-3' 
+            onSubmit={handleSubmit(async (data) => 
+            {
+                await axios.post('/api/issues', data)
+                router.push('/issues')
+            }
+            )}>
+            <TextField.Root placeholder='Title' {...register('title')}></TextField.Root>
+            <Controller 
+                name="description"
+                control={control}
+                render={( { field }) => <SimpleMDE placeholder='Description' {...field}/>}
+            />
+
             <Button>Submit New Issue</Button>
-        </div>
+        </form>
     )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
           <main className="p-5">
             {children}
           </main>
-          <ThemePanel />
+          {/* <ThemePanel /> */}
         </Theme>
       </body>
     </html>
