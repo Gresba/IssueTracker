e48fc6fd2da3d9536f9ba69ca336eba328fbad05
Paul Kim <kimpaul0709@gmail.com>
2025-10-06 21:29:56 -0400

    Add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,14 +1,18 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import { prisma } from '@/prisma/client'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
-import React from 'react'
+
+import ReactMarkDown from 'react-markdown'
 
 interface Props {
-    params: { id: string }
+    params: Promise<{ id: string }>
 }
 
-const IssueDetailPage = async ({ params: { id } }: Props) => {
-    if(typeof id !== 'number') notFound();
-
+const IssueDetailPage = async ({ params }: Props) =>
+{
+    const { id } = await params;
+    
     const issue = await prisma.issue.findUnique({
         where: {id: parseInt(id)}
     })
@@ -17,10 +21,14 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
 
     return (
         <div>
-            <p>{issue.title}</p>
-            <p>{issue.description}</p>
-            <p>{issue.status}</p>
-            <p>{issue.createdAt.toDateString()}</p>
+            <Heading>{issue.title}</Heading>
+            <Flex className='space-x-3' my="2">
+                <IssueStatusBadge status={issue.status}/>
+                <Text>{issue.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card className='prose' mt="4">
+                <ReactMarkDown>{issue.description}</ReactMarkDown>
+            </Card>
         </div>
     )
 }
