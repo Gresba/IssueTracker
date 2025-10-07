9d1659139a84fce6a74c4c07f5c6a9f665d8e894
Paul Kim <kimpaul0709@gmail.com>
2025-10-07 17:46:39 -0400

    Built Edit issue page

---
+++ app/issues/components/IssueForm.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import { Button, Callout, TextField } from '@radix-ui/themes'
+import { useForm, Controller } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import dynamic from 'next/dynamic';
+import axios from 'axios'
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import { Issue } from '@/app/generated/prisma';
+import "easymde/dist/easymde.min.css";
+
+// Dynamically import SimpleMDE with no SSR
+const SimpleMDE = dynamic(
+    () => import('react-simplemde-editor'),
+    { ssr: false }
+);
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+const IssueForm = ({issue}: {issue?: Issue}) => {
+
+    const router = useRouter();
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueFormData>({
+        resolver: zodResolver(createIssueSchema),
+    });
+
+    const [ formSubmmited, setFormSubmitted ] = useState(false);
+    const [ error, setError ] = useState("");
+
+    return (
+        <div className='max-w-xl'>
+            {error && <Callout.Root color='red' className='mb-5'>
+                <Callout.Text>
+                    {error}
+                </Callout.Text>
+            </Callout.Root>}
+            <form
+                className='space-y-3 mt-3'
+                onSubmit=
+                {
+                    handleSubmit(async (data) => {
+                    try
+                    {
+                        setFormSubmitted(true)
+                        await axios.post('/api/issues', data);
+                        router.push('/issues');
+                    } catch (error) {
+                        setFormSubmitted(false)
+                        setError('An unexpected error occurred.');
+                    }
+                }
+                )}>
+                <TextField.Root defaultValue={issue?.title} placeholder='Title' {...register('title')}></TextField.Root>
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
+                <Controller
+                    name="description"
+                    control={control}
+                    defaultValue={issue?.description}
+                    render={({ field }) => (
+                        <SimpleMDE 
+                            placeholder='Description' 
+                            {...field} />)}
+                />
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
+                <Button disabled={formSubmmited}>{formSubmmited ? "Submitting" : "Submit"} New Issue {formSubmmited && <Spinner/>} </Button>
+            </form>
+        </div>
+    )
+}
+
+export default IssueForm
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,75 +1,10 @@
-'use client'
+import React from 'react'
+import IssueForm from '../components/IssueForm'
 
-import { Button, Callout, TextField } from '@radix-ui/themes'
-import { useForm, Controller } from 'react-hook-form';
-import { useRouter } from 'next/navigation';
-import dynamic from 'next/dynamic';
-import axios from 'axios'
-import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod';
-
-import "easymde/dist/easymde.min.css";
-import ErrorMessage from '@/app/components/ErrorMessage';
-import Spinner from '@/app/components/Spinner';
-
-// Dynamically import SimpleMDE with no SSR
-const SimpleMDE = dynamic(
-    () => import('react-simplemde-editor'),
-    { ssr: false }
-);
-
-type IssueForm = z.infer<typeof createIssueSchema>;
-
-const NewIssuesPage = () => {
-
-    const router = useRouter();
-    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
-        resolver: zodResolver(createIssueSchema)
-    });
-
-    const [ formSubmmited, setFormSubmitted ] = useState(false);
-    const [ error, setError ] = useState("");
-
-    return (
-        <div className='max-w-xl'>
-            {error && <Callout.Root color='red' className='mb-5'>
-                <Callout.Text>
-                    {error}
-                </Callout.Text>
-            </Callout.Root>}
-            <form
-                className='space-y-3 mt-3'
-                onSubmit=
-                {
-                    handleSubmit(async (data) => {
-                    try
-                    {
-                        setFormSubmitted(true)
-                        await axios.post('/api/issues', data);
-                        router.push('/issues');
-                    } catch (error) {
-                        setFormSubmitted(false)
-                        setError('An unexpected error occurred.');
-                    }
-                }
-                )}>
-                <TextField.Root placeholder='Title' {...register('title')}></TextField.Root>
-                <ErrorMessage>{errors.title?.message}</ErrorMessage>
-                <Controller
-                    name="description"
-                    control={control}
-                    render={({ field }) => (
-                        <SimpleMDE 
-                            placeholder='Description' 
-                            {...field} />)}
-                />
-                <ErrorMessage>{errors.description?.message}</ErrorMessage>
-                <Button disabled={formSubmmited}>{formSubmmited ? "Submitting" : "Submit"} New Issue {formSubmmited && <Spinner/>} </Button>
-            </form>
-        </div>
-    )
+const NewIssuePage = () => {
+  return (
+    <IssueForm />
+  )
 }
 
-export default NewIssuesPage
+export default NewIssuePage
